3613da57a2683a86283f79d75a8ad43ef74d54d7
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2026-01-08 19:00:03 +0100

    feat: finito esercizio

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 import express from 'express'
 import ArticlesRouter from './routers/articoli.js'
+
 const app = express()
 const port = 7000
 
 app.use(express.json())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("miao")
 })
 
-app.use("/articoli",ArticlesRouter)
- 
-app.listen(port, function(){
-    console.log("il server è in ascolto sulla porta" + port)
+app.use("/articoli", ArticlesRouter)
+
+app.use((req, res) => {
+    res.status(404).json({ 
+        errore: "Endpoint non trovato", 
+        metodo: req.method, 
+        percorso: req.originalUrl 
+    });
+});
+
+
+
+app.use((err, req, res, next) => {
+    console.error("Errore:", err.stack);
+    res.status(500).json({ 
+        errore: "Errore interno del server", 
+        messaggio: err.message 
+    });
+});
+
+app.listen(port, function() {
+    console.log("il server è in ascolto sulla porta " + port)
 })
+
